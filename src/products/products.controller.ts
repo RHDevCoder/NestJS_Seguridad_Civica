6317451a2caaf857ef32b9a98f3a07c5708e30bd
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-07-27 21:36:00 -0500

    459 Actualizar productos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -39,8 +39,11 @@ findAll(@Query() query: GetProductsQueryDTO) {
     return this.productsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  @Put(':id')
+  update(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
