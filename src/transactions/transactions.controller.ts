a7513b69b232691466f93bf9b516128b82762ae4
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-07-30 00:18:18 -0500

    475 Eliminar ventas por su id

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -31,7 +31,7 @@ export class TransactionsController {
 */
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', IdValidationPipe) id: string) {
     return this.transactionsService.remove(+id);
   }
 }
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -13,68 +13,67 @@ export class TransactionsService {
     @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
     @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>,
     @InjectRepository(Product) private readonly productRepository: Repository<Product>,
-  ){}
-
+  ) {}
 
   async create(createTransactionDto: CreateTransactionDto) {
-  await this.productRepository.manager.transaction(async (transactionEntityManager) => {
-    const transaction = new Transaction();
-    transaction.total = createTransactionDto.contents.reduce((total, item) => total + (item.quantity * item.price), 0)
+    await this.productRepository.manager.transaction(async (transactionEntityManager) => {
+      const transaction = new Transaction();
+      transaction.total = createTransactionDto.contents.reduce((total, item) => total + (item.quantity * item.price), 0);
 
-    await transactionEntityManager.save(transaction);
+      await transactionEntityManager.save(transaction);
 
-    for (const contents of createTransactionDto.contents) {
-  const product = await transactionEntityManager.findOneBy(Product, { id: contents.productId });
-  const errors: string[] = [];
+      for (const contents of createTransactionDto.contents) {
+        const product = await transactionEntityManager.findOneBy(Product, { id: contents.productId });
+        const errors: string[] = [];
 
-  if (!product) {
-    errors.push(`El producto con el ID: ${contents.productId} no existe`);
-    throw new NotFoundException(errors);
+        if (!product) {
+          errors.push(`El producto con el ID: ${contents.productId} no existe`);
+          throw new NotFoundException(errors);
+        }
+
+        if (contents.quantity > product.inventory) {
+          errors.push(`El artículo ${product.name} excede la cantidad disponible`);
+          throw new BadRequestException(errors);
+        }
+
+        product.inventory -= contents.quantity;
+        await transactionEntityManager.save(product);
+
+        const transactionContent = new TransactionContents();
+        transactionContent.price = contents.price;
+        transactionContent.quantity = contents.quantity;
+        transactionContent.transaction = transaction;
+        transactionContent.product = product;
+
+        await transactionEntityManager.save(transactionContent);
+      }
+    });
+
+    return "Venta almacenada correctamente";
   }
-
-  if (contents.quantity > product.inventory) {
-    errors.push(`El artículo ${product.name} excede la cantidad disponible`);
-    throw new BadRequestException(errors);
-  }
-
-  product.inventory -= contents.quantity;
-  await transactionEntityManager.save(product);
-
-  const transactionContent = new TransactionContents();
-  transactionContent.price = contents.price;
-  transactionContent.quantity = contents.quantity;
-  transactionContent.transaction = transaction;
-  transactionContent.product = product;
-
-  await transactionEntityManager.save(transactionContent);
-}
-  });
-
-  return "Venta almacenada correctamente";
-}
 
   findAll(transactionDate?: string) {
-  const options: FindManyOptions<Transaction> = {
-    relations: {
-      contents: true
+    const options: FindManyOptions<Transaction> = {
+      relations: {
+        contents: true
+      }
+    };
+
+    if (transactionDate) {
+      const date = parseISO(transactionDate);
+      if (!isValid(date)) {
+        throw new BadRequestException('Fecha no valida');
+      }
+
+      const start = startOfDay(date);
+      const end = endOfDay(date);
+
+      options.where = {
+        transactionDate: Between(start, end)
+      };
     }
+    return this.transactionRepository.find(options);
   }
-
-  if(transactionDate) {
-    const date = parseISO(transactionDate)
-    if(!isValid(date)) {
-      throw new BadRequestException('Fecha no valida')
-    }
-
-    const start = startOfDay(date)
-    const end = endOfDay(date)
-
-    options.where = {
-      transactionDate: Between(start, end)
-    }
-  }
-  return this.transactionRepository.find(options);
-}
 
   async findOne(id: number) {
     const transaction = await this.transactionRepository.findOne({
@@ -84,20 +83,35 @@ export class TransactionsService {
       relations: {
         contents: true
       }
-    })
+    });
 
-    if(!transaction) {
-      throw new NotFoundException('Transaccion no encontrada')
+    if (!transaction) {
+      throw new NotFoundException('Transaccion no encontrada');
     }
-    
-    return transaction
+
+    return transaction;
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+  // SE DEJARA PARA LA PARTE FINAL DEL PROYECTO
+  // update(id: number, updateTransactionDto: UpdateTransactionDto) {
+  //   return `This action updates a #${id} transaction`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async remove(id: number) {
+    const transaction = await this.findOne(id);
+
+    if (!transaction) {
+      throw new NotFoundException(`Transacción con id ${id} no encontrada`);
+    }
+
+    // Elimina primero los contents
+    for (const content of transaction.contents) {
+      await this.transactionContentsRepository.remove(content);
+    }
+
+    // Luego elimina la transacción
+    await this.transactionRepository.remove(transaction);
+
+    return {message: 'Venta eliminada'}
   }
 }
