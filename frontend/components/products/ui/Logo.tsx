4d74a7f44da7b8e671b48b5e8c903d1658f3a610
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-03 22:33:38 -0500

    504 Creando el layout de la tienda

---
+++ frontend/components/products/ui/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+    return (
+        <h1 className="text-3xl font-extrabold text-white">SEGURIDAD {''}
+        <span className="text-green-400 text-xl ">CIVICA</span>
+        </h1>
+    )
+}
