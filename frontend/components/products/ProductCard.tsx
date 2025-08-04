5793e962e84dd078fb38e9b155a28a9d0a7e6cb8
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-03 22:18:17 -0500

    503 Mostrando las imagenes de los productos

--- frontend/components/products/ProductCard.tsx
+++ frontend/components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/src/schemas";
 import { formatCurrency } from "@/src/utils";
+import Image from "next/image";
 
 export default function ProductCard({product} : {product : Product}) {
     return (
@@ -7,7 +8,12 @@ export default function ProductCard({product} : {product : Product}) {
             className='rounded bg-white shadow relative p-5'
         >
             <div>
-            
+                <Image
+                    src={`${process.env.API_URL}/img/${product.image}`}
+                    alt={`Imagen de producto ${product.name}`}
+                    width={400}
+                    height={600}                
+                />
                 <div className="p-3 space-y-2">
                     <h3 className="text-xl font-bold text-gray-600">{product.name}</h3>
                     <p className="text-gray-500">Disponibles: {product.inventory}</p>
