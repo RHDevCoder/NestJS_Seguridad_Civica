f85f6fbdc90dc20ec8fb0e8c94fb122dd430694a
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-05 22:47:16 -0500

    509 Generando el schema de carrito

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -14,9 +14,23 @@ export const CategorySchema = z.object({
     name: z.string()
 })
 
+/** Shopping Cart */
+const ShoppingCartContentsSchema = ProductSchema.pick({
+    name: true,
+    image: true,
+    price: true,
+    inventory: true,
+}).extend({
+    productId:z.number(),
+    quantity: z.number()
+})
+export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema)
+
+
 export const CategoriesRespondeSchema = z.array(CategorySchema)
 export const CategoryWithProductsResponseSchema = CategorySchema.extend({
     products: z.array(ProductSchema)
 });
 
 export type Product = z.infer<typeof ProductSchema>
+export type ShoppingCart = z.infer<typeof ShoppingCartSchema>
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,14 +1,17 @@
 import { create } from "zustand";
-import { Product } from "./schemas";
+import { devtools } from 'zustand/middleware'
+import { Product, ShoppingCart } from "./schemas";
 
 interface Store {
     total : number
+    contents: ShoppingCart
     addToCart: (product: Product) => void
 }
 
-export const useStore = create<Store>(() => ({
+export const useStore = create<Store>()(devtools((set, get) => ({
     total: 0,
+    contents: [],
     addToCart: (product) => {
         console.log(product)
     }
-}))
+})))
