2d6d33459e781158118686438ceadc04269980cf
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-07 11:53:56 -0500

    510 Añadiendo productos al carrito

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -12,6 +12,29 @@ export const useStore = create<Store>()(devtools((set, get) => ({
     total: 0,
     contents: [],
     addToCart: (product) => {
-        console.log(product)
+        const { id: productId, categoryId, ...data } = product
+        let contents : ShoppingCart = []
+        const duplicated = get().contents.findIndex(item => item.productId === productId)
+
+        if(duplicated >= 0) {
+
+            if(get().contents[duplicated].quantity >= get().contents[duplicated].inventory ) return
+
+            contents = get().contents.map(item => item.productId === productId ? {
+                ...item,
+                quantity: item.quantity + 1
+            } : item )
+
+        } else {
+            contents = [...get().contents, {
+                ...data,
+                quantity: 1,
+                productId
+            }]
+        }
+
+        set (() => ({
+            contents
+        }))
     }
 })))
