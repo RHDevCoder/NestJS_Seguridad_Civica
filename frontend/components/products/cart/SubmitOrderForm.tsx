fbb05a59392ecd429224c10f84fbc11799b366e2
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-10 00:03:46 -0500

    532 Limpiar el carrito despues de una venta

--- frontend/components/products/cart/SubmitOrderForm.tsx
+++ frontend/components/products/cart/SubmitOrderForm.tsx
@@ -9,6 +9,8 @@ export default function SubmitOrderForm() {
     const total = useStore(state => state.total)
     const coupon = useStore(state => state.coupon.name)
     const contents = useStore(state => state.contents)
+    const clearOrder = useStore(state => state.clearOrder)
+
     const order = {
         total,
         coupon,
@@ -27,6 +29,7 @@ export default function SubmitOrderForm() {
         }
         if(state.success) {
             toast.success(state.success)
+            clearOrder()
         }
     }, [state])
 
