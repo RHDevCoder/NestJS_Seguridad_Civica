bf57bad91453f93e0a9619b82cfebf719a824742
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-10 15:22:39 -0500

    538 Formateando la fecha como iso

--- frontend/components/transactions/TransactionFilter.tsx
+++ frontend/components/transactions/TransactionFilter.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import Calendar from "react-calendar"
 import"react-calendar/dist/Calendar.css"
+import {format} from 'date-fns'
 
 type ValuePiece = Date | null
 type Value = ValuePiece | [ValuePiece, ValuePiece]
@@ -11,7 +12,8 @@ export default function TransactionFilter() {
 
     const [date, setDate] = useState<Value>(new Date())
 
-    console.log(date)
+    const formattedDate = format(date?.toString()!, 'yyyy-MM-dd')
+    console.log(formattedDate)
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
