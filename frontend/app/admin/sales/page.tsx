3d3425899ff9dbe23c1b15862e82b60f25e152ad
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-10 15:38:20 -0500

    539 Configurando react query en next.js

--- frontend/app/admin/sales/page.tsx
+++ frontend/app/admin/sales/page.tsx
@@ -1,14 +1,22 @@
 import TransactionFilter from "@/components/transactions/TransactionFilter";
 import Heading from "@/components/ui/Heading";
+import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default function SalesPage() {
+export default async function SalesPage() {
+    const queryClient = new QueryClient()
+
+    await queryClient.prefetchQuery({
+        queryf
+    })
+
     return (
         <>
             <Heading>Ventas</Heading>
             <p className="text-lg">En esta seccion apareceran las ventas, utiliza el calendario para filtrar por fecha</p>
 
-
+        <HydrationBoundary state={dehydrate(queryClient)}>
             <TransactionFilter />
+        </HydrationBoundary>
         </>
     )
 }
