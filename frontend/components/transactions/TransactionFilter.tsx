da72e1c376fa77236ca657f571968ae9168b8cfe
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-10 21:23:52 -0500

    546 Calculando el total vendido del dia

--- frontend/components/transactions/TransactionFilter.tsx
+++ frontend/components/transactions/TransactionFilter.tsx
@@ -7,6 +7,7 @@ import {format} from 'date-fns'
 import { useQuery } from "@tanstack/react-query"
 import { getSalesByDate } from "@/src/api"
 import TransactionSummary from "./TransactionSummary"
+import { formatCurrency } from "@/src/utils"
 
 type ValuePiece = Date | null
 type Value = ValuePiece | [ValuePiece, ValuePiece]
@@ -19,6 +20,8 @@ export default function TransactionFilter() {
         queryKey: ['sales', formattedDate],
         queryFn: () => getSalesByDate(formattedDate)
     })
+
+    const total = data?.reduce((total, transaction) => total + +transaction.total, 0 ) ?? 0
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
@@ -38,6 +41,10 @@ export default function TransactionFilter() {
                         transaction={transaction}
                     />
                 )) : <p className="text-lg text-center">No hay ventas en esta fecha</p> : null}
+
+                <p className="my-5 text-xl font-bold text-right">Total del dia: {''}
+                    <span className="font-normal">{formatCurrency(total)}</span>
+                </p>
             </div>
         </div>
     )
