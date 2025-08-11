1a428851ac2a036efde6aff5452b5ddc0f2dccd8
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-10 22:54:14 -0500

    549 Mostrando los productos en una tabla

--- frontend/app/admin/products/page.tsx
+++ frontend/app/admin/products/page.tsx
@@ -1,3 +1,4 @@
+import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { ProductsResponseSchema } from "@/src/schemas";
 
@@ -5,17 +6,23 @@ async function getProducts() {
     const url = `${process.env.API_URL}/products`
     const req = await fetch(url)
     const json = await req.json()
-    const products = ProductsResponseSchema.parse(json)
-    return products
+    const data = ProductsResponseSchema.parse(json)
+    return {
+        products: data.products,
+        total: data.total
+    }
 }
 
 export default async function ProductsPage() {
-    const products = await getProducts()
+    const {products, total} = await getProducts()
 
     return (
         <>
             <Heading>Administrar productos</Heading>
 
+            <ProductsTable
+                products={products}
+            />
         </>
     )
 }
