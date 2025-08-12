c68b8a716a024c1f7337476939f22c1cee066697
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-11 23:00:30 -0500

    550 Validando desde la url para añadir paginacion

--- frontend/app/admin/products/page.tsx
+++ frontend/app/admin/products/page.tsx
@@ -1,9 +1,11 @@
 import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { ProductsResponseSchema } from "@/src/schemas";
+import { isValidPage } from "@/src/utils";
+import { redirect } from "next/navigation";
 
-async function getProducts() {
-    const url = `${process.env.API_URL}/products`
+async function getProducts(take : number, skip : number) {
+    const url = `${process.env.API_URL}/products?take=${take}&skip=${skip}`
     const req = await fetch(url)
     const json = await req.json()
     const data = ProductsResponseSchema.parse(json)
@@ -13,8 +15,17 @@ async function getProducts() {
     }
 }
 
-export default async function ProductsPage() {
-    const {products, total} = await getProducts()
+type SearchParams = Promise<{page: string}>
+
+export default async function ProductsPage({searchParams} : {searchParams: SearchParams}) {
+
+    const {page} = await searchParams
+
+    if(!isValidPage(+page)) redirect('/admin/products?page=1')
+
+    const productsPerPage = 10
+    const skip = (+page - 1) * productsPerPage
+    const {products, total} = await getProducts(productsPerPage, skip)
 
     return (
         <>
