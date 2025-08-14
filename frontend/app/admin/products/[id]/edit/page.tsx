d3425bd6dd9e730d6dc1e57656f6e2ac10217f1c
RHDevCoder <ricardoherreralopez84@gmail.com>
2025-08-14 00:05:14 -0500

    558 Consultando la api para obtener el producto a editar

--- frontend/app/admin/products/[id]/edit/page.tsx
+++ frontend/app/admin/products/[id]/edit/page.tsx
@@ -1,9 +1,30 @@
 import EditProductForm from "@/components/products/EditProductForm";
 import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
+import { ProductSchema } from "@/src/schemas";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
-export default function EditProductPage() {
+async function getProduct(id: string) {
+    const url = `${process.env.API_URL}/products/${id}`
+    const req = await fetch(url)
+    const json = await req.json()
+    if(!req.ok) {
+        notFound()
+    }
+    const product = ProductSchema.parse(json)
+    return product
+}
+
+type Params = Promise<{id: string}>
+
+export default async function EditProductPage({params} : {params : Params}) {
+
+        const {id} = await params
+        const product = await getProduct(id)
+
+        console.log(product)
+
     return (
         <>
             <Link
